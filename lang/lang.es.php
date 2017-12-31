cbd342f36980a327ca534a284f61bb62ecae9223
Akintunde102 <jegedeakintunde@gmail.com>
2017-12-31 08:04:46 +0100

    Update lang.es.php

--- lang/lang.es.php
+++ lang/lang.es.php
@@ -13,12 +13,5 @@ $lang['SITE_NAME'] = 'Mi Sitio Web';
 $lang['SLOGAN'] = 'Mi lema aquí';
 $lang['HEADING'] = 'Título';
 
-// Menu
 
-$lang['MENU_HOME'] = 'Inicio';
-$lang['MENU_ABOUT_US'] = 'Sobre Nosotros';
-$lang['MENU_OUR_PRODUCTS'] = 'Nuestros productos';
-$lang['MENU_CONTACT_US'] = 'Contáctenos';
-$lang['MENU_ADVERTISE'] = 'Publicidad';
-$lang['MENU_SITE_MAP'] = 'Mapa del Sitio';
 ?>
